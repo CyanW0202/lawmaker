ea9a869811349da9a7e07f80b30ad36b968a89a7
CyanW0202 <47878471+CyanW0202@users.noreply.github.com>
2021-09-25 16:41:00 -0700

    Create lawmaker_h.h

---
+++ lawmaker_h.h
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class Senate{
+    public:
+        char list[5][100] = {"Thursday: Mr. Walker invited you to have a drink.","",""};
+};
+
+class the_House{
+    public:
+        char list[5][100] = {"Mr. Walker invited you to help his reelection next month.","",""};
+};
