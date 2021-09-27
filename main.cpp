8a5707f81e5bc07e83f8e5ec4bed58a998904c50
CyanW0202 <47878471+CyanW0202@users.noreply.github.com>
2021-09-26 21:44:34 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,11 +8,20 @@ Chia
 Ver1
 *******************************************************************************/
 
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <iostream>
 #include <unistd.h>
 #include "lawmaker_h.h"
 using namespace std;
 int plot(int plotChoice, int count);
+bool party();
 int plot_count = 0;
 int plot_choice = 0;
 int role = 0;
@@ -20,7 +29,7 @@ Senate S1;
 the_House H1;
 int main()
 {
-    cout<<"Choose a character: (1)for being a senator; (2) for being a member of the House ";
+    cout<<"Choose a character for being a parliamentarian from \n(1)the Senate \n(2)the House ";
     cin>>role;
     plot_choice = plot(role, plot_count);
     for(int i =1; i<5; i++){
@@ -34,9 +43,7 @@ int main()
 int plot(int plotChoice, int count){
     cout<<"You are sitting in a car, and your calendar is in your hand...\n";
     usleep(1000000);
-    switch (plot_count){
-        case 0:
-            switch(plotChoice){
+    switch(role){
                 case 1:
                     for(int i =0; i<5; i++){
                         cout<< "("<< i+1 <<") "<<S1.list[i]<<"\n";
@@ -45,19 +52,57 @@ int plot(int plotChoice, int count){
                 
                 case 2:
                     for(int i =0; i<6; i++){
-                        cout<< S1.list[i];
+                        cout<< "("<< i+1 <<") "<< H1.list[i]<<"\n";
                     }
                     break;
             }
-            cin>>plotChoice;
-            plot_count++;
+    cin>>plotChoice;
+    plot_count++;        
+    switch (plotChoice){
+        case 0:
             return plotChoice;
             break;
         case 1:
+            if(party()){
+                cout<<"This is the day.";
+                 cout<<"The party you join is the majority.";
+                 cout<<"They voted over 2/3.";
+                 sleep(3);
+                 cout<<"The Congress Passed your proposal.";
+            }else{
+                cout<<"This is the day.";
+                cout<<"The party you join is the majority.";
+                cout<<"They voted over 2/3.";
+                sleep(3);
+                cout<<"The Congress Passed your proposal.";
+            }
+        
             break;
         case 2:
             break;
             
     }
     return 0;
+}
+bool party(){
+    int plotChoice = 0;
+    cout<<"You saw the president was sitting with the chair.\n";
+    sleep(1);
+    cout<<"The president asked you if you are the same party with the chair:\n";
+    sleep(1);
+    cout<<"(1)Yes! She is like my model. I always wish if I can organized my idea like she did.\n";
+    cout<<"(2)She is persuasive, although we are not the same party sometimes I want to agree with her personally.\n";
+    cin>>plotChoice;
+    if (plotChoice>200){
+        return true;
+    }
+    else if(plotChoice>100){
+        return false;
+    }
+    else{
+        cout<<" 'You should not say that... let's try this again.' the narrator said.\n";
+        return party();
+    }
+    
+
 }
