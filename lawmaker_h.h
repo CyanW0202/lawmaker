4a5ba2b5b0d537717264125049ab6be4be289eac
CyanW0202 <47878471+CyanW0202@users.noreply.github.com>
2021-09-26 23:23:36 -0700

    Update lawmaker_h.h

--- lawmaker_h.h
+++ lawmaker_h.h
@@ -3,10 +3,11 @@ using namespace std;
 
 class Senate{
     public:
-        char list[5][100] = {"Thursday: Mr. Walker invited you to have a drink.","",""};
+        char list[3][100] = {"Thursday: Mr. Walker invited you to have a drink before his relection.","",""};
 };
 
 class the_House{
     public:
-        char list[5][100] = {"Mr. Walker invited you to help his reelection next month.","",""};
+        char list[3][100] = {"Thursday: Meeting with Mr. Walker for maintaining water systems in Cypress.",
+        "",""};
 };
